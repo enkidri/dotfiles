51677b0346ec78183344b57f1ff2f0ab423c3129
Tai Ta <tai.ta@toyota-industries.eu>
2025-02-28 17:06:09 +0100

    Update

--- setup.sh
+++ setup.sh
@@ -6,10 +6,16 @@ set -e
 SOURCE_DIRECTORY=$(dirname "$(realpath "$0")")
 
 # Symbolic link for neovim
+# NOTE: Will likely require more configuration outside this
 rm -rf $TARGET_DIRECTORY/nvim
 ln -sf "${SOURCE_DIRECTORY}/nvim" ~/.config
 
 # Symbolic link for tmux
+if [ ! -d "~/.tmux/plugins/tpm" ]; then
+  mkdir -p ~/.tmux/plugins/tpm
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
 ln -sf "${SOURCE_DIRECTORY}/tmux/.tmux.conf" ~
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
 
 echo "Setup completed successfully!"
