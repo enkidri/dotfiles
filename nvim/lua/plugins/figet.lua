e06293ffad7b6dec48e205665169f934c51a20aa
ArchMinion <lawliet@hotmail.se>
2026-01-03 16:43:22 +0100

    Made popup look nicer

---
+++ nvim/lua/plugins/figet.lua
@@ -0,0 +1,12 @@
+-- lua/plugins/fidget.lua
+return {
+	"j-hui/fidget.nvim",
+	opts = {
+		notification = {
+			window = {
+				winblend = 20, -- transparency
+				border = "rounded",
+			},
+		},
+	},
+}
